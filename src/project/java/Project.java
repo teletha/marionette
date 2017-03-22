80263907f221eeb5674c61db3f0bf02ae027b804
Teletha <Teletha.T@gmail.com>
2017-03-23 01:26:47 +0900

    Initial commit.

---
+++ src/project/java/Project.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2017 Marionette Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+public class Project extends bee.api.Project {
+
+    {
+        product("com.github.teletha", "Marionette", "0.1");
+
+        require("com.github.teletha", "sinobu", "1.0");
+        require("net.java.dev.jna", "jna", "4.2.2");
+        require("net.java.dev.jna", "jna-platform", "4.2.2");
+    }
+}
