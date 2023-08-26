34463c46cbca9773a8d29758e9dfb25411bd3e66
Teletha <Teletha.T@gmail.com>
2023-08-26 14:59:10 +0900

    add lol

---
+++ src/test/java/marionette/LoL.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2023 The MARIONETTE Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
+package marionette;
+
+public class LoL extends Macro<LoL> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void declare() {
+        require(window -> window.title().contains("League of Legends"), () -> {
+            whenPress(Key.A).to(() -> {
+                input(Key.P).delay(300).input(Key.O);
+            });
+        });
+    }
+}
