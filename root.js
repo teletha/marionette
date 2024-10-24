3fc5f2fb0ced913f29e14b37c706e6ffbb046a50
teletha <teletha@users.noreply.github.com>
2024-10-24 01:03:47 +0000

    deploy: 575c98fd34cfb35182b3253b3065efe5f5ad9465

---
+++ root.js
@@ -0,0 +1,73 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"marionette",
+		"marionette.platform"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Color",
+			"packageName": "marionette.platform",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Key",
+			"packageName": "marionette",
+			"type": "Enum"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "KeyEvent",
+			"packageName": "marionette",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Location",
+			"packageName": "marionette.platform",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Macro",
+			"packageName": "marionette",
+			"type": "AbstractClass"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "MacroOption",
+			"packageName": "marionette",
+			"type": "Enum"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Mouse",
+			"packageName": "marionette",
+			"type": "Enum"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Native",
+			"packageName": "marionette.platform",
+			"type": "Interface"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Window",
+			"packageName": "marionette",
+			"type": "Class"
+		}
+	]
+}
